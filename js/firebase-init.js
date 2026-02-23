e7688d9cc921a1d1d00080b2eb3f4cd523c1369c
Michael. <patersonseanmichael@gmail.com>
2026-02-23 21:57:11 +1000

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,1 +1,19 @@
+// js/firebase-init.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 
+// ✅ PASTE YOUR FIREBASE CONFIG HERE:
+const firebaseConfig = {
+  apiKey: "PASTE_HERE",
+  authDomain: "PASTE_HERE",
+  projectId: "PASTE_HERE",
+  storageBucket: "PASTE_HERE",
+  messagingSenderId: "PASTE_HERE",
+  appId: "PASTE_HERE"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// expose auth globally for other modules
+window.vvAuth = auth;
